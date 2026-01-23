e4fc3b2a1ed0a72af8aa7df54b2c9db099bf741d
aakalewii <erlewiirobot@gmail.com>
2026-01-23 16:19:19 +0000

    modelos anuncio, conversacion, mensaje

--- relink/app/Models/Conversacion.php
+++ relink/app/Models/Conversacion.php
@@ -3,8 +3,49 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Conversacion extends Model
 {
-    //
+    protected $table = 'conversaciones';
+
+    protected $fillable = [
+        'anuncio_id',
+        'vendedor_id',
+        'comprador_id',
+        'estado'
+    ];
+
+    /**
+     * Relación con el anuncio del que se está hablando
+     */
+    public function anuncio(): BelongsTo
+    {
+        return $this->belongsTo(Anuncio::class, 'anuncio_id');
+    }
+
+    /**
+     * Relación con el vendedor
+     */
+    public function vendedor(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'vendedor_id');
+    }
+
+    /**
+     * Relación con el comprador
+     */
+    public function comprador(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'comprador_id');
+    }
+
+    /**
+     * Relación con los mensajes que contiene esta charla
+     */
+    public function mensajes(): HasMany
+    {
+        return $this->hasMany(Mensaje::class, 'conversacion_id');
+    }
 }
--- relink/app/Models/Mensaje.php
+++ relink/app/Models/Mensaje.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Mensaje extends Model
 {
-    //
+    protected $table = 'mensajes';
+
+    protected $fillable = [
+        'conversacion_id',
+        'contenido',
+        'fecha_envio',
+        'remitente_id',
+        'fecha_envio',
+    ];
+
+    /**
+     * Relación con el anuncio del que se está hablando
+     */
+    public function conversacion(): BelongsTo
+    {
+        return $this->belongsTo(Conversacion::class, 'conversacion_id');
+    }
+
+    /**
+     * Relación con el vendedor
+     */
+    public function remitente(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'remitente_id');
+    }
+
 }
