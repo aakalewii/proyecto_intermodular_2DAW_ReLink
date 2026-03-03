65361b1cb17ba8387c8d9d9454c4e1ed98392769
alvaro123124 <alvarobudy12@gmail.com>
2026-03-03 16:14:41 +0000

    subacategoiar

--- relink/app/Http/Controllers/SubcategoriaController.php
+++ relink/app/Http/Controllers/SubcategoriaController.php
@@ -3,8 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Subcategoria;
 
-class SubcategoriaController extends Controller
+class SubcategoriasController extends Controller
 {
-    //
+    public function index()
+    {
+        $subcategorias = Subcategoria::with('categoria')->get();
+        return response()->json($subcategorias);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nombre' => ['required', 'string', 'max:255'],
+            'descripcion' => ['required', 'string', 'max:255'],
+            'categoria_id' => ['required', 'integer', 'exists:categorias,id'], 
+        ]);
+
+        $subcategoria = Subcategoria::create($validated);
+        
+        return response()->json([
+            'message' => 'Subcategoría creada con éxito',
+            'data' => $subcategoria
+        ], 201);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $subcategoria = Subcategoria::find($id);
+
+        if (!$subcategoria) {
+            return response()->json([
+                'message' => 'Subcategoría no encontrada'
+            ], 404);
+        }
+
+        $validated = $request->request->validate([
+            'nombre' => ['required', 'string', 'max:255'],
+            'descripcion' => ['required', 'string', 'max:255'],
+            'categoria_id' => ['required', 'integer', 'exists:categorias,id'],
+        ]);
+
+        $subcategoria->update($validated);
+        
+        return response()->json([
+            'message' => 'Actualizada con éxito',
+            'data' => $subcategoria
+        ], 200);
+    }
+
+    public function destroy(Request $request, int $id)
+    {
+        $subcategoria = Subcategoria::find($id);
+
+        if (!$subcategoria) {
+            return response()->json([
+                'message' => 'Subcategoría no encontrada'
+            ], 404);
+        }
+
+        $subcategoria->delete();
+        
+        return response()->json([
+            'message' => 'Eliminada con éxito'
+        ], 200);
+    }
 }
